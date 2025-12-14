6438dc5d62004dd69c472ed1f3a84951fdddf1e5
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-12-14 10:23:08 +0500

    update userRoute to add routes for all users and profile image update

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { userProfile } = require("../controllers/userProfileController.js");
+const { getAllUsers, updateProfileImage  } = require("../controllers/userController.js");
+
 
 router.get("/profile/:cms",  userProfile);
+router.get("/all",           getAllUsers);
+router.put("/profile-image", updateProfileImage);
 
 module.exports = router;
