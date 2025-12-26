9420f671effe8ac1cba5c6ba778a3a2b03fd2068
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-12-26 10:42:53 +0500

    feat: add auth routes and default login redirect

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-
 import './index.css'
 
+import Login from './Pages/auth/Login.jsx'
 import Register from './Pages/auth/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/auth/register" />} />
+        <Route path="/" element={<Navigate to="/auth/login" />} />
 
+        <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
 
       </Routes>
