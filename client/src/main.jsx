0d922687b0e5c0311506ad1aac6a552dd8d2d090
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2026-01-01 05:47:31 +0500

    feat: add protected route and wall page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Login from './Pages/auth/Login.jsx'
 import Register from './Pages/auth/Register.jsx'
+
+import Wall from "./Pages/wall/wall.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +18,16 @@ createRoot(document.getElementById('root')).render(
 
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
+
+        {/* Protected Route*/}
+        <Route
+          path="/wall"
+          element={
+            <ProtectedRoute allowedRoles={["Male", "Female"]}>
+              <Wall />
+            </ProtectedRoute>
+          }
+        />
 
       </Routes>
     </Router>
